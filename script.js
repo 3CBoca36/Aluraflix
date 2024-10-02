cef76fc38e3e8137cbff2ed373a493f8217a7f2b
3CBoca36 <00001102954718sp@al.educacao.sp.gov.br>
2024-10-02 12:35:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Lucas Roberto Rodrigues 3C 21/09/2024
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
